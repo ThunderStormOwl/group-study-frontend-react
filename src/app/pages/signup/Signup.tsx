76394ab2145f22eaa878a0d7e1a14ca8cedccc21
ThunderStormOwl <lausrock@gmail.com>
2021-02-20 19:55:04 -0300

    Added form component

--- src/app/pages/signup/Signup.tsx
+++ src/app/pages/signup/Signup.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { ThemeContext } from '../../shared/contexts/theme';
 import { useTheme } from '../../shared/hooks/useTheme';
+import Form from '../../shared/components/Form';
 
 export const Signup: React.FC = () => {
 
     const {isDark} = useTheme();
 
     return(
-        <div>
-            Sign Up
+        <div className="form-wrapper">
+            
         </div>
     );
 
---
+++ src/app/shared/components/Form.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+interface FormProps extends React.FormHTMLAttributes<HTMLFormElement>{
+    formClass: string;
+}
+
+export const Form: React.FC<FormProps> = (props) => {
+
+    return(
+        <form
+            {...props}
+            className={props.formClass}
+        />
+    );
+
+}
