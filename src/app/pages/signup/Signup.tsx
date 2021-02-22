a1b48e629347e07df45ab8694308a086542e459f
ThunderStormOwl <lausrock@gmail.com>
2021-02-22 00:52:01 -0300

    Buttons finished styling and trying to handle data from the form.

--- src/app/pages/signup/Signup.tsx
+++ src/app/pages/signup/Signup.tsx
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form } from '../../shared/components/Form';
 import { Input } from'../../shared/components/Input';
 import './Signup.css';
 import { DarkModeCheckbox } from '../../shared/components/DarkModeCheckbox';
 import { Button } from '../../shared/components/Button';
 
+const formDataTemplate = {
+    name:"",
+    email:"",
+    username:"",
+    password:"",
+    phone:"",
+    address:""
+}
+
 export const Signup: React.FC = () => {
-    
+
+    const [formData, updateFormData] = React.useState(formDataTemplate);
+
+    const handleFormChange = ((e) => {
+
+        updateFormData({
+            ...formData,
+            [e.target.name] : e.target.value.trim()
+        });
+
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(formData);
+        //send to back end here
+    };
 
     return(
         <div className="page-wrapper">
@@ -16,11 +41,13 @@ export const Signup: React.FC = () => {
                     <Input
                         label="Full name:"
                         placeholder="Phil Becker"
+                        onChange={handleFormChange}
                     />
                     <Input 
                         label="Email:"
                         placeholder="philb123@email.com"
                         type="email"
+                        onChange={handleFormChange}
                     />
                 </div>
 
@@ -28,11 +55,13 @@ export const Signup: React.FC = () => {
                     <Input
                         label="Username:"
                         placeholder="phil.is.Cool_123"
+                        onChange={handleFormChange}
                     />
                     <Input 
                         label="Password:"
                         placeholder="123imgay;)"
                         type="password"
+                        onChange={handleFormChange}
                     />
                 </div>
 
@@ -40,10 +69,12 @@ export const Signup: React.FC = () => {
                     <Input 
                         label="Phone number:"
                         placeholder="555-12345"
+                        onChange={handleFormChange}
                     />
                     <Input 
                         label="Address:"
                         placeholder="123 whatever street"
+                        onChange={handleFormChange}
                     />
                 </div>
                 
@@ -59,7 +90,11 @@ export const Signup: React.FC = () => {
                     </div>
 
                     <div className="submit-button-wrapper">
-                        <Button className="form-button test">
+                        <Button 
+                            className="form-button test"
+                            type="submit"
+                            onClick={handleSubmit}
+                        >
                             Sign me up!
                         </Button>
                     </div>
