def917ae3056c6d5ecb4f713321603e151a77813
ThunderStormOwl <lausrock@gmail.com>
2021-04-24 09:57:42 -0300

    cross-env fix

--- src/app/shared/services/axios-config/AxiosConfig.ts
+++ src/app/shared/services/axios-config/AxiosConfig.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const Api = axios.create({
-    baseURL : 'https://quiconsulta-backend-nodejs-v48ot.ondigitalocean.app'
+    baseURL : process.env.REACT_API_URL || 'https://localhost:3333/'
 })
