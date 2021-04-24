e30be2bac204e9544e7e552df1645f5284509da3
ThunderStormOwl <lausrock@gmail.com>
2021-04-24 10:29:04 -0300

    small change

--- src/app/shared/services/axios-config/AxiosConfig.ts
+++ src/app/shared/services/axios-config/AxiosConfig.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const Api = axios.create({
-    baseURL : process.env.REACT_API_URL || 'https://localhost:3333/'
+    baseURL : process.env.REACT_API_URL || 'https://localhost:3333'
 })
