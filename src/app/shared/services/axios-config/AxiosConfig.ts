077e5b467efb85507f536b1e08e57c6fff2df7bd
ThunderStormOwl <lausrock@gmail.com>
2021-04-25 01:29:10 -0300

    put the backend address as static value, needs to change later to environment variable!

--- src/app/shared/services/axios-config/AxiosConfig.ts
+++ src/app/shared/services/axios-config/AxiosConfig.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const Api = axios.create({
-    baseURL : process.env.REACT_API_URL || 'https://localhost:3333'
+    baseURL : 'https://quiconsulta-backend-nodejs-v48ot.ondigitalocean.app/'//process.env.REACT_API_URL || 'https://localhost:3333'
 })
