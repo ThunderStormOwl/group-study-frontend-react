4a9ac3b85a413d983b077ff024216f5fad4b39c7
ThunderStormOwl <lausrock@gmail.com>
2021-05-01 08:41:22 -0300

    API URL var correctly set up

--- src/app/shared/services/axios-config/AxiosConfig.ts
+++ src/app/shared/services/axios-config/AxiosConfig.ts
@@ -1,5 +1,6 @@
+import { Environment } from './../../../environment/Environment';
 import axios from 'axios';
 
 export const Api = axios.create({
-    baseURL : 'https://quiconsulta-backend-nodejs-v48ot.ondigitalocean.app/'//process.env.REACT_API_URL || 'https://localhost:3333'
+    baseURL : Environment.URL_API,
 })
